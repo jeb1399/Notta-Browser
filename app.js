ca53153c78e06d377bc2da262536fcb9b809800c
takenusername <145628605+jeb1399@users.noreply.github.com>
2024-09-23 03:53:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ var unblocker = new Unblocker({ prefix: '/proxy/' });
 app.use(unblocker);
 app.use(express.static('public'));
 app.get('/', function(req, res) {res.sendFile(__dirname + '/index.html');});
+app.get('/robots.txt', function(req, res) {res.sendFile(__dirname + '/robots.txt');});
 app.listen(process.env.PORT || 8080).on('upgrade', unblocker.onUpgrade);
 console.log("Node Unblocker Server Running On Port:", process.env.PORT || 8080);
